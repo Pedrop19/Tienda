261515819a0d2c82ec5813624904c7a3555a6ae8
Pedrop19 <prlazaron01@iesalbarregas.es>
2024-02-05 18:41:14 +0100

    Añadido estilos de Bootstrap y librería SweetAlert2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +10,25 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Tienda';
+
+  alerta(title: string, text: string) {
+    Swal.fire({
+      title: "Desea añadir el producto: " + title + "?",
+      text: text,
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, añadir!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Confirmado!",
+          text: "El producto ha sido añadido al carrito!",
+          icon: "success",
+          confirmButtonColor: "#3085d6",
+        });
+      }
+    });
+  }
 }
